c9d1799d4444ffc57bcb07021af5e13b097d5589
nicolas.ddm <nicolasdenckerdemarco@gmail.com>
2020-09-14 07:13:51 -0300

    finish all tests

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -54,12 +54,26 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', {
+      id,
+    });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const loadedFoods = await api.get('/foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
+
+      setFoods(
+        loadedFoods.data.map((food: Food) => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        })),
+      );
     }
 
     loadFoods();
@@ -67,7 +81,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const loadedCategories = await api.get('/categories');
+      setCategories(loadedCategories.data);
     }
 
     loadCategories();
@@ -75,6 +90,9 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+    } else setSelectedCategory(id);
   }
 
   return (
